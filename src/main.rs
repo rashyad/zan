9e65eb74c110c4f0c91bb3b554806eacbfe0f5a5
rashyad <rashyadarfan@gmail.com>
2024-04-17 03:21:14 +0400

    Implemented tera template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+mod tera;
+
+use rocket::response::content::RawHtml;
+use rocket_dyn_templates::Template;
+
+#[get("/")]
+fn index() -> RawHtml<&'static str> {
+    RawHtml(r#"See <a href="tera">Tera</a> or <a href="hbs">Handlebars</a>."#)
+}
+
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/tera", routes![tera::index, tera::hello, tera::about])
+        .register("/tera", catchers![tera::not_found])
+        .attach(Template::custom(|engines| {
+            tera::customize(&mut engines.tera);
+        }))
+
 }
